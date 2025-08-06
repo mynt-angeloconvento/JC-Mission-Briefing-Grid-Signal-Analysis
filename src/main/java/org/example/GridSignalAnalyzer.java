12d1ab83755659746afa145648befe860f8427db
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-08-06 15:08:28 +0800

    feat: add input

--- src/main/java/org/example/GridSignalAnalyzer.java
+++ src/main/java/org/example/GridSignalAnalyzer.java
@@ -2,5 +2,16 @@ package org.example;
 
 public class GridSignalAnalyzer {
     public static void main(String[] args) {
+
+        GridSignalAnalyzer analyzer = new GridSignalAnalyzer();
+
+        // Mission Data: The Scrambled Communications Grid
+        char[][] scrambledGrid = {
+                { 'C', 'H', 'A', 'R', 'L', 'I', 'E' },
+                { 'A', 'L', 'P', 'H', 'A' },
+                { 'T', 'A', 'N', 'G', 'O', '7' },
+                { 'B', 'R', 'A', 'V', 'O' }
+        };
+
     }
 }
